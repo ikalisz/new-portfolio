6341ea558bdcd26d79d2fcf69a32cba207cc5ef1
Ian Kaliszewski <ikalisz@yahoo.com>
2020-09-10 21:53:49 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+} from '@material-ui/core/styles'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Page>
+        
+      </Page>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+const Page = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    overflow-x: hidden;
+    padding-right: 0;
+  }
+`
